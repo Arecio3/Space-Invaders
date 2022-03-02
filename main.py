6d2c93953fbe2f24a63f1aaf9e69e28c27092f3c
Arecio Canton <devmain@Arecios-iMac.local>
2022-03-02 01:38:51 -0500

    loading in assets

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 import pygame
+import os
+import time
+import random
+
+# Load Images
+Red_Space_Ship = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+Blue_Space_Ship = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+Green_Space_Ship = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+# Player Ship
+Yellow_Space_Ship = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
+
+# Lasers
+Red_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+Blue_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+Green_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+Yellow_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+
+# Background
+Background = pygame.image.load(os.path.join("assets", "background-black.png"))
