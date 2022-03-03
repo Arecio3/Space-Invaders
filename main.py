b1f76ddb9f6f87cba029dd3fbf2eddfe0f5ffc5e
Arecio Canton <areciocodes@gmail.com>
2022-03-02 20:23:49 -0500

    fixed naming conevtion

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ WIDTH, HEIGHT = 750, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # Load Images
-Red_Space_Ship = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
-Blue_Space_Ship = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
-Green_Space_Ship = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+BlUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
 # Player Ship
-Yellow_Space_Ship = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
 
 # Lasers
-Red_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
-Blue_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
-Green_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
-Yellow_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
 # Background
-Background = pygame.image.load(os.path.join("assets", "background-black.png"))
+BACKGROUND = pygame.image.load(os.path.join("assets", "background-black.png"))
