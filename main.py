cb3b7a06e745dfcfc6ee21d4df89b957c155dc3b
Arecio Canton <areciocodes@gmail.com>
2022-03-02 20:32:47 -0500

    working on main loop

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import random
 WIDTH, HEIGHT = 750, 750
 # Create Screen
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Shoot That Shit")
 
 # Load Images
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
@@ -22,3 +23,20 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 
 # Background
 BACKGROUND = pygame.image.load(os.path.join("assets", "background-black.png"))
+
+# Main loop to run game logic
+def main():
+    run = True
+    FPS = 60
+    clock = pygame.time.Clock()
+
+    while run:
+        # Tick clock based on FPS Rate (Allows our game to stay consistent on any device)
+        clock.tick(FPS)
+        # Check for events 
+        for event in pygame.event.get():
+            # If user exits stop the game
+            if event.type == pygame.QUIT:
+                run = False
+
+main()
